8913f673f7eebc42dabec4e7ab73b830a7885074
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-03-08 18:02:25 +0000

    Divide Kp for position by 2.35

--- SpinningDiskController/SpinningDiskController.ino
+++ SpinningDiskController/SpinningDiskController.ino
@@ -84,7 +84,7 @@ float motorPrimaryOffsetPos = 0; //set in setup()
 float motorPrimaryOffsetNeg = 0; //set in setup()
 
 // POSITION
-static float plantForPosition[] = {-1,+1}; // was: max error is when half a revolution away
+static float plantForPosition[] = {-0.42553191489,+0.42553191489}; 
 static float driveForPosition[] = {-1,1}; // max 50% drive
 static int sizePosition = 2;
 Driver driverPosition = Driver(plantForPosition, driveForPosition, sizePosition);
